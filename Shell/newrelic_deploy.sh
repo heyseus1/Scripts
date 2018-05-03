5b6342519d2dcd3923ce74e46b58b3c438a710a1
Matthew M <mmorcaldi99@gmail.com>
2018-05-02 22:03:17 -0700

    removing key

--- Shell/newrelic_deploy.sh
+++ Shell/newrelic_deploy.sh
@@ -15,7 +15,7 @@ install_deps() {
 check_debian() {
 	if [ -n "$(cat /etc/*-release | grep -i 'ID=debian')" ] ; then
 		os=`cat /etc/*-release | grep 'VERSION' | awk '{print $2}' |  sed -e "s/(//" | sed -e 's/)"//' | perl -ne 'print lc'`
-		echo "license_key: 26952a025640e073b97cd75045963b06014823f8" | tee -a /etc/newrelic-infra.yml
+		echo "license_key: insert key here" | tee -a /etc/newrelic-infra.yml
 		curl https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | apt-key add -
 		printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt `echo ${os}` main" | \
 		sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
@@ -29,7 +29,7 @@ check_debian() {
 check_ubuntu() {
 	if [ -n "$(cat /etc/*-release | grep -i 'ID=ubuntu')" ] ; then
 		os=`cat /etc/*-release | grep 'VERSION' | awk '{print $3}' | sed -e "s/(//" | perl -ne 'print lc'`
-		echo "license_key: 26952a025640e073b97cd75045963b06014823f8" | tee -a /etc/newrelic-infra.yml
+		echo "license_key: insert key here" | tee -a /etc/newrelic-infra.yml
 		curl https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | apt-key add -
 		printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt `echo ${os}` main" | \
 		sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
@@ -43,7 +43,7 @@ check_ubuntu() {
 
 check_amazon() {
 	if [ -n "$(cat /etc/*-release | grep -i 'ID="amzn"')" ] ; then
-		echo "license_key: 26952a025640e073b97cd75045963b06014823f8" | sudo tee -a /etc/newrelic-infra.yml
+		echo "license_key: insert key here" | sudo tee -a /etc/newrelic-infra.yml
 		curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/6/x86_64/newrelic-infra.repo
 		yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
 		yum install newrelic-infra -y
@@ -54,7 +54,7 @@ check_amazon() {
 
 check_centos7() {
 	if [ -n "$(cat /etc/*-release | grep -i 'ID="centos"')" ] ; then
-		echo "license_key: 26952a025640e073b97cd75045963b06014823f8" | sudo tee -a /etc/newrelic-infra.yml
+		echo "license_key: insert key here" | sudo tee -a /etc/newrelic-infra.yml
 		curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/7/x86_64/newrelic-infra.repo
 		yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
 		yum install newrelic-infra -y
@@ -65,7 +65,7 @@ check_centos7() {
 
 check_centos6() {
 	if [ -n "$(cat /etc/*-release | grep -i 'CentOS release 6')" ] ; then
-		echo "license_key: 26952a025640e073b97cd75045963b06014823f8" | sudo tee -a /etc/newrelic-infra.yml
+		echo "license_key: insert key here" | sudo tee -a /etc/newrelic-infra.yml
 		curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/6/x86_64/newrelic-infra.repo
 		yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
 		yum install newrelic-infra -y
